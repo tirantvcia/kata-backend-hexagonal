84722d9727a67493fea395daf63e76415ded529a
tirantvcia <tirantvcia@gmail.com>
2024-04-07 13:53:17 +0200

    refactor, move classes to core package

---
+++ src/core/Id.ts
@@ -0,0 +1,28 @@
+import { ValidationError } from './ValidationError';
+import { generateUuuid } from './common/generateUuuid';
+
+
+export class Id {
+
+    private constructor(readonly uuid: string) { }
+    public static generate() {
+        return new Id(generateUuuid());
+    }
+    public static createFrom(id: string) {
+        Id.validateUuidFormat(id);
+        return new Id(id);
+    }
+    private static validateUuidFormat(id: string) {
+        const regex: RegExp = /^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$/;
+        const isNotValidFormat = !regex.test(id.toString());
+        if (isNotValidFormat) {
+            throw new ValidationError("id must have a valid format");
+        }
+    }
+    public toString() {
+        return this.uuid;
+    }
+    isEqual(id: Id): boolean {
+        return this.uuid === id.toString();
+    }
+}
--- src/tests/unit/id.test.ts
+++ src/tests/unit/id.test.ts
@@ -1,34 +1,7 @@
-import {v4 as uuidv4} from 'uuid';
 import { ValidationError } from '../../core/ValidationError';
+import { Id } from '../../core/Id';
+import { generateUuuid } from '../../core/common/generateUuuid';
 
-export function generateUuuid(): string {
-    return uuidv4();
-}
-
-export class Id {
-
-    private constructor(readonly uuid: string){}
-    public static generate() {
-        return new Id(generateUuuid());
-    }
-    public static createFrom(id: string) {
-        Id.validateUuidFormat(id);
-        return new Id(id);
-    }
-    private static validateUuidFormat(id: string) {
-        const regex: RegExp = /^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$/;
-        const isNotValidFormat = !regex.test(id.toString());
-        if(isNotValidFormat) {
-            throw new ValidationError("id must have a valid format");
-        }
-    }
-    public toString() {
-        return this.uuid;
-    }
-    isEqual(id: Id): boolean {
-        return this.uuid === id.toString();
-    }
-}
 describe('The Id', () => {
     it('generate a valid identifier', () => {
         const id = Id.generate();
