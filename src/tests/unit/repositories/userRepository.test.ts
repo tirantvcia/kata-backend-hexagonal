8fb9710787e44337f6025d9af80ebdf4bf88b794
tirantvcia <tirantvcia@gmail.com>
2024-04-18 14:42:42 +0200

    refactor test, extract repo creation in beforeEach method

--- src/tests/unit/repositories/userRepository.test.ts
+++ src/tests/unit/repositories/userRepository.test.ts
@@ -5,10 +5,15 @@ import { Id } from "../../../core/valuelObjects/Id"
 import { Password } from "../../../core/valuelObjects/Password";
 
 describe('Te in memory user repository', () => {
+    let userRepository: InMemoryRepository ;
+
+    beforeEach(() => {
+        userRepository = new InMemoryRepository();
+    });
+
     it('finds an user by id', async () => {
         const id = Id.generate();
         const user = createUserById(id);
-        const userRepository: InMemoryRepository = new InMemoryRepository();
         userRepository.save(user);
         const userFromBBDD = await userRepository.findById(id);
         expect(userFromBBDD).toBe(user);
@@ -16,7 +21,6 @@ describe('Te in memory user repository', () => {
     })
     it('does not find a non existing user by id', async () => {
         const id = Id.generate();
-        const userRepository: InMemoryRepository = new InMemoryRepository();
         const userFromBBDD = await userRepository.findById(id);
         expect(userFromBBDD).toBe(undefined);
         
@@ -24,7 +28,6 @@ describe('Te in memory user repository', () => {
     it('finds an user by email', async () => {
         const email = Email.create('test@hexagonal.com');
         const user = createUserByEmail(email);
-        const userRepository: InMemoryRepository = new InMemoryRepository();
         userRepository.save(user);
         const userFromBBDD = await userRepository.findByEmail(email);
         expect(userFromBBDD).toBe(user);
@@ -32,7 +35,6 @@ describe('Te in memory user repository', () => {
     })
     it('does not find a non existing user by email', async () => {
         const email = Email.create('test@hexagonal.com');
-        const userRepository: InMemoryRepository = new InMemoryRepository();
         const userFromBBDD = await userRepository.findByEmail(email);
         expect(userFromBBDD).toBe(undefined);
     })
@@ -41,7 +43,6 @@ describe('Te in memory user repository', () => {
         const user = createUserByEmail(email);
         const anotherEmail = Email.create('test1@hexagonal.com');
         const anotherUser = createUserByEmail(anotherEmail);
-        const userRepository: InMemoryRepository = new InMemoryRepository();
         userRepository.save(user);
         userRepository.save(anotherUser);
         const usersFromBBDD = await userRepository.findAll();
